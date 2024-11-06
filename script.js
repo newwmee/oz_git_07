74eb7cba6c74c8d2377f87d04f4bdf5ed462ed72
newwmee <dmstjs365@gmail.com>
2024-11-06 10:39:23 +0900

    🐛 계산기 오류 처리 기능 추가
    잘못된 수식 입력시 try-catch 구문을 통해 안전한 계산처리를 위한 안내 멘트 작성

--- script.js
+++ script.js
@@ -13,5 +13,9 @@ function appendToDisplay(value) {
 }
 
 function calculateResult() {
-  display.innerText = eval(display.innerText); // 오류 처리 없이 eval 사용
+  try {
+    display.innerText = eval(display.innerText); // 오류 처리 없이 eval 사용
+  } catch {
+    display.innerText = "숫자와 사칙연산 기호만 입력해주세요";
+  }
 }
